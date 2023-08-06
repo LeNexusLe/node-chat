dee2c5e225683ae9df236729997a1b9b79aef2ad
Kacper Papiernik <nexus.steam@o2.pl>
2023-08-06 17:10:15 +0200

    simple chat

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 const socket = io('http://localhost:3000');
+const messageContainer = document.getElementById('message-container');
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('message-input');
 
 socket.on('chat-message', (data) => {
-  console.log(data);
+  appendMessage(`${data.name}: ${data.message}`);
 });
 
-const sendContainer = document.getElementById('send-container');
-const input = document.getElementById('message-input');
-
-sendContainer.addEventListener('submit', (e) => {
+messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  socket.emit('test');
+  const message = messageInput.value;
+  appendMessage(`You: ${message}`);
+  socket.emit('send-chat-message', message);
+  messageInput.value = '';
 });
 
-const costam = (element, id, text, cos) => {};
+const appendMessage = (message) => {
+  const messageEmlement = document.createElement('div');
+  messageEmlement.innerText = message;
+  messageContainer.append(messageEmlement);
+};
+
+const name = prompt('What is your name?');
+appendMessage('You Joined');
+socket.emit('new-user', name);
+
+socket.on('user-connected', (name) => {
+  appendMessage(`${name} connected`);
+});
+socket.on('user-disconnected', (name) => {
+  appendMessage(`${name} disconnected`);
+});
